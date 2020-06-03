9caf92cd1cac5fa03db81c194fd6f2c82c9194f4
landon tipantiza <landontipantiza@gmail.com>
2020-06-03 03:39:27 +0000

    fixed bugs

--- timer1.js
+++ timer1.js
@@ -3,12 +3,11 @@ let inputs = process.argv.slice(2);
 const timer = (inputs) => {
   for (const input of inputs){
     const number = Number(input);
-    if(!(number === NaN)){
-      if (number >= 0){
-        setTimeout(() => {
-          process.stdout.write('\x07');
-        }, input * 1000);
-      }
+    if (number >= 0){ 
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, input * 1000);
+      
     } 
   }
 }
